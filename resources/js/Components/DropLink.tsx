525a0ccb5063cfbad9b4f1355a199a54c2ede18b
Sergo <S.prihodchenko@yandex.ru>
2024-05-02 23:21:34 +0300

    a

--- resources/js/Components/DropLink.tsx
+++ resources/js/Components/DropLink.tsx
@@ -3,9 +3,9 @@ import { NavDropdown } from "react-bootstrap";
 
 export default function DropLink({name}: {name:string}) {
     return (
-        <NavDropdown title={name} id="navbarScrollingDropdown" className="navbar_toggle_name">
+        <NavDropdown title={name} id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Divider />
+              <NavDropdown.Divider/>
               <NavDropdown.Item>
                 <Link method="post" href={route('logout')}>
                     Logout
--- resources/js/Pages/Main.tsx
+++ resources/js/Pages/Main.tsx
@@ -11,11 +11,26 @@ export default function Main({ auth,  appName}: PageProps<{ appName: string}>) {
             <GuestLayout>
                <Container className='content_box'>
                     <Container className='time_table'> 
+                    <h2>Наши услуги</h2>
                         <Row>
-                            <h1>Свободное время</h1>
+                            <ul className='present_list'>
+                                <li>Наши услуги</li>
+                                <li>Наши услуги</li>
+                                <li>Наши услуги</li>
+                            </ul>
                         </Row>
                         <Row>
                             
+                        </Row>
+                    </Container>
+                    <Container className='time_table'>
+                        <h2>Время работы</h2>
+                        <Row>
+                            <ul className='present_list'>
+                                <li>пн-пт: 8:00 - 19:00</li>
+                                <li>сб: 10:00 - 17:00</li>
+                                <li>вб: выходной</li>
+                            </ul>
                         </Row>
                     </Container>
                </Container>
